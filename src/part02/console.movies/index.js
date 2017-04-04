015590980e9a95b5df600ab913a148dd74193f3f
johnnynode <johnnynode@gmail.com>
2017-04-04 21:56:10 +0800

    格式整理

--- src/part02/console.movies/index.js
+++ src/part02/console.movies/index.js
@@ -6,14 +6,14 @@ let num = 0;
 // 加入数组
 (() => {
     for (var i = 0; i < 14; i++) {
-        arr.push(fs.readFileSync(path.resolve(__dirname, 'frames/' + (i + 1) + '.txt')));
+        arr.push(fs.readFileSync(path.resolve(__dirname, 'frames', (i + 1) + '.txt')));
     }
 })();
 
 // 进行读写
 (() => {
     setInterval(function () {
-        process.stdout.write('\033c');
+        process.stdout.write('\033c'); // 将当前控制台清空
         num++;
         if (num === arr.length) num = 0;
         process.stdout.write(arr[num]);
