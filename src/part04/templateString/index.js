0ddff34a476ed753f42b1cefbd5ead1d703c2a9a
johnnynode <johnnynode@gmail.com>
2017-04-09 20:32:55 +0800

    add String.raw method

--- src/part04/templateString/index.js
+++ src/part04/templateString/index.js
@@ -1,17 +1,18 @@
 "use strict";
 
-let foo = "hello";
-let res = `
-    ${foo}
-    ${getWorld()} 
-    \n
-    ${foo}`
-function getWorld() {
-    return "world";
-}
-console.log("res：");
-console.log(res);
-
+(() => {
+    let foo = "hello";
+    let res = `
+        ${foo}
+        ${getWorld()} 
+        \n
+        ${foo}`
+    function getWorld() {
+        return "world";
+    }
+    console.log("res：");
+    console.log(res);
+})();
 /*
 输出结果为：
 res：
@@ -21,3 +22,9 @@ res：
     
     hello
  */
+
+(() => {
+    let num = 1;
+    let res = String.raw`${num + 2}haha`;
+    console.log(res); // 3haha
+})();
