e3e272db77b903f90320710876c09360ce6f4b67
johnnynode <johnnynode@gmail.com>
2017-04-13 09:40:41 +0800

    整理

--- src/part02/visitSpeedTest/index.js
+++ src/part02/visitSpeedTest/index.js
@@ -1,27 +1,29 @@
+"use strict";
+
 const request = require('request');
 
-// 简单的测试就可以
-testTaoBao();
-testQQ();
-testBaidu();
-
-function testTaoBao() {
-    console.time('t');
-    request('taobao.com', () => {
-        console.timeEnd('t');
-    })
-}
-
-function testQQ() {
-    console.time('q');
-    request('qq.com', () => {
-        console.timeEnd('q');
-    });
-}
-
-function testBaidu() {
+// 测试 baidu
+(() => {
     console.time('b');
     request('baidu.com', () => {
         console.timeEnd('b');
     });
-}
+})();
+
+// 测试 taobao
+(() => {
+    console.time('a');
+    request('taobao.com', () => {
+        console.timeEnd('a');
+    })
+})();
+
+// 测试 qq
+(() => {
+    console.time('t');
+    request('qq.com', () => {
+        console.timeEnd('t');
+    });
+})();
+
+// 经过代码的修改和测试，发现在放在最后的一组请求总是最快的，比如现在的qq, 用时最少
