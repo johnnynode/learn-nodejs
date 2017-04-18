9dc6afce459a543053796cac69ade8e4e2fe61dd
johnnynode <johnnynode@gmail.com>
2017-04-18 12:38:13 +0800

    整理，并添加fs.open用于读写文件

---
+++ src/part05/moreAboutFs/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const fs = require('fs');
+const path = require('path');
+const fullPath = path.resolve(__dirname, 'index.js');
+
+// 测试验证路径是否存在 exists 已不推荐使用 (NOT RECOMMENDED)
+(() => {
+    // 异步操作
+    fs.exists(fullPath, (exists) => {
+        console.log('exists: ' + exists); // exists: true
+    });
+
+    // 同步验证
+    let exists = fs.existsSync(fullPath);
+    console.log('sync exists: ' + exists); // sync exists: true
+})();
+
+// fs.open 通过第二个参数的传递来确定是读还是写。
+(()=>{
+    let fullPath1 = path.join(__dirname,'myfile.txt');
+    let fullPath2 = path.join(__dirname,'myfile2.txt');
+    // write
+    fs.open(fullPath, 'wx', (err, fd) => {
+        if (err) {
+            if (err.code === "EEXIST") return console.error('myfile already exists');
+            throw err;
+        }
+        console.log('do your write operation!'); // 然后可以做写文件操作了。
+    });
+
+    // read
+    fs.open(fullPath2, 'r', (err, fd) => {
+        if (err) {
+            if (err.code === "ENOENT") return console.error('myfile does not exist');        
+            throw err;
+        }
+        console.log('do your read operation!');
+    });
+})();
+
+
+// 测试文件信息
+(() => {
+    
+})();
+
+// 测试删除文件
+(() => {
+
+})();
