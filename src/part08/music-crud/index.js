556e03a17d2dc2c414be40b2b70dd1ab028628fd
johnnynode <johnnynode@gmail.com>
2017-05-25 08:44:26 +0800

    finish add deit delete

--- src/part08/music-crud/index.js
+++ src/part08/music-crud/index.js
@@ -10,6 +10,7 @@ const musicList = require('./musicData'); // 获取数据
 
 // 用来匹配删除链接的url
 const regex_remove = /^\/remove\/(\d{1,6})$/;
+const regex_edit = /^\/edit\/(\d{1,6})$/;
 
 const server = http.createServer((req, res) => {
     let urlObj = url.parse(req.url);
@@ -67,10 +68,55 @@ const server = http.createServer((req, res) => {
     } else if (method === 'GET' && regex_remove.test(pathname)) {
         let m_id = pathname.match(regex_remove)[1];
         let index = musicList.findIndex(m => m.id === m_id);
-        console.log(index);
+        // console.log(index);
         musicList.splice(index, 1);
-        res.end('remove success');
-    } else if (method === 'GET' && pathname === '/error') {
+        // 删除完成, 回到首页
+        res.writeHead(301, {
+            'Location': '/'
+        });
+        res.end();
+    } else if (method === 'GET' && regex_edit.test(pathname)){
+        let m_id = pathname.match(regex_edit)[1];
+        // 根据音乐信息id找到该项
+        let musicInfo = musicList.find(m => m.id === m_id);
+        // 如果不存在，返回该项不存在。
+        if (!musicInfo) {
+            return res.end('music is not exists');
+        }
+        // 读取文件
+        fs.readFile(path.join(__dirname, 'edit.html'), 'utf8', function (err, data) {
+            if (err) {
+                return res.end(err.message);
+            }
+            // 拿到数据之后，模板编译
+            let compiled = _.template(data);
+            let htmlStr = compiled({
+                musicInfo
+            });
+            res.writeHead(200, {
+                'Content-Type': 'text/html; charset=utf-8'
+            });
+            res.end(htmlStr);
+        });
+    } else if(method === 'POST' && regex_edit.test(pathname)){
+        // 处理post提交的数据
+        recivePostData(req, function (requestBody) {
+            let m_id = pathname.match(regex_edit)[1];
+            let name = requestBody.name;
+            let singer = requestBody.singer;
+            let isHightRate = requestBody.isHightRate;
+            // 你要编辑谁？
+            // 根据id查找数组中的索引
+            let index = musicList.findIndex(m => m.id === m_id);
+            musicList[index].name = name;
+            musicList[index].singer = singer;
+            musicList[index].isHightRate = isHightRate === '1';
+            res.writeHead(301, {
+                'Location': '/'
+            });
+            res.end();
+        });
+    } else if (pathname === '/error') {
         fs.readFile( path.join(__dirname, '404.html'), 'utf8', (err, data) => {
             if (err) {
                 return res.end(err.message);
