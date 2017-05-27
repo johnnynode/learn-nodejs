c0981ca1b919d861b3d8dd2a6268cf0018f2d15f
johnnynode <johnnynode@gmail.com>
2017-05-27 08:27:59 +0800

    纠正错误,使用content-length 无法判断为POST请求,必须method是POST才行, 添加依赖项

--- src/part08/music-crud2/services/body-parser.js
+++ src/part08/music-crud2/services/body-parser.js
@@ -6,8 +6,6 @@ const qstring = require('querystring'); // querystring模块
 module.exports = function (req, next) {
   // 当请求进来之后，先看看有没有请求体，如果有，动态的解析出来，解析完后挂载到req对象上
   // 如果没有，继续向后执行
-  // 如果请求行中有 content-length ，表示是通过post方式提交的表单 这句话是明显错误的
-  // 直接使用POST
   if (req.method === 'POST') {
     let data = '';
     req.on('data', function (chunk) {
