eb5ca2241de758a2a80647d0e24bfb3d171b8e0c
johnnynode <johnnynode@gmail.com>
2018-03-18 10:42:45 +0800

    optimize post request

--- src/part06/advancedRequest/server/index.js
+++ src/part06/advancedRequest/server/index.js
@@ -7,14 +7,19 @@ var startServer = function (router, list) {
   var onRequest = function (req, res) {
     var method = req.method;
     var pathname = url.parse(req.url).pathname;
-  
-    var data = "";
+    
+    var data = [];
     req.on("error", function(err) {
         console.error(err);
     }).on("data", function(chunk) {
-        data += chunk;
+        data.push(chunk);
     }).on('end', function() {
         if (method === "POST") {
+          if(data.length > 1e6) {
+            console.log('too much post, server will close it');
+            return req.connection.destroy();
+          }
+          data = Buffer.concat(data).toString();
           var params = querystring.parse(data);
           router(list, pathname, res, params);
         } else if(method === "GET") {
