411c5206dd0438e7020074f15b2fad21ce4d280e
johnnynode <johnnynode@gmail.com>
2018-03-18 11:34:43 +0800

    test express send and json method

--- src/part06/express-server/server.js
+++ src/part06/express-server/server.js
@@ -6,6 +6,19 @@ app.get('/', function (req, res) {
   res.send('home page');
 });
 
+app.get('/json', (req,res)=>{
+  var json = [
+    {
+      name:"John"
+    },
+    {
+      name:"Lily"
+    }
+  ];
+  // res.send(json);
+  res.json(json);
+})
+
 app.listen(3000,"127.0.0.1", function () {
   console.log('server is listening at port 3000');
 });
