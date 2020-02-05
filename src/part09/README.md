be1c170713cc06281252d505d0470b5a87a48fc6
johnnynode <johnnynode@gmail.com>
2020-02-05 15:09:01 +0800

    update: part09 epress-router-pro

--- src/part09/README.md
+++ src/part09/README.md
@@ -163,3 +163,58 @@ app.search('/', function(req, res) {
 ```
 
 其他method方式可以类似支持
+
+### 路由的其他相关应用
+
+接收参数
+
+```js
+app.get('/user/:name/:group', function(req, res, next) {
+  let name = req.params.name;
+  let group = req.params.group;
+
+  console.log(name, group);
+  // next();
+});
+```
+
+创建路由对象
+
+```js
+var router = express.Router({
+    mergeParams: true, // 配置父级路由信息是否共享, 默认false
+    caseSensitive: true, // 配置路由的路径大小写是否区分, 默认false
+    strict: true // 配置是否区分严格模式 /test 和 /test/是完全不一样的
+});
+
+// 也可以使用下面app.set的方式来配置
+// app.set('strict routing', true);
+// app.set('case sensitive routing', true);
+
+app.get('/user/:name/:group', function(req, res, next) {
+  let name = req.params.name;
+  let group = req.params.group;
+
+  console.log(name, group);
+  next();
+});
+
+app.use('/user/:name/:group', router);
+
+router.get('/', function(req, res) {
+    res.send('router info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/test', function(req, res) {
+    res.send('router test info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/TEst', function(req, res) {
+    res.send('router TEst info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/test/', function(req, res) {
+    res.send('router test/ info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+```
--- src/part09/express-router-pro/app.js
+++ src/part09/express-router-pro/app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,22 +12,49 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.use(methodOverride('__method', {methods:['POST', 'GET']})); // 配置中间件
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride(function(req, res) {
-  return req.body.__method;
-})); // 配置中间件
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.search('/', function(req, res) {
-  res.send('my name is search method.');
+var router = express.Router({
+  mergeParams: true, // 配置父级路由信息是否共享, 默认false
+  caseSensitive: true, // 配置路由的路径大小写是否区分, 默认false
+  strict: true // 配置是否区分严格模式 /test 和 /test/是完全不一样的
 });
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// 也可以使用下面app.set的方式来配置
+// app.set('strict routing', true);
+// app.set('case sensitive routing', true);
+
+app.get('/user/:name/:group', function(req, res, next) {
+let name = req.params.name;
+let group = req.params.group;
+
+console.log(name, group);
+next();
+});
+
+app.use('/user/:name/:group', router);
+
+router.get('/', function(req, res) {
+  res.send('router info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/test', function(req, res) {
+  res.send('router test info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/TEst', function(req, res) {
+  res.send('router TEst info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
+
+router.get('/test/', function(req, res) {
+  res.send('router test/ info: ' + req.params.name + ' ' + req.params.group); // 上面配置过即可获取信息
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
