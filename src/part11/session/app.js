488f2876111ebeaf4f9c2fecd3a942c8504ef360
johnnynode <johnnynode@gmail.com>
2020-02-11 00:02:31 +0800

    update: part11 session

--- src/part11/session/app.js
+++ src/part11/session/app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 app.use(cookieParser());
 app.use(session({
+    store: new FileStore(), // 这个配置是基于session-file-store的, 服务器重启后 默认将文件保存到./sessions文件夹下
     secret: "Joh",
-    cookie:{maxAge: 20*1000} // 20s的有效时间
+    cookie:{maxAge: 20*1000} // 20s的有效时间 过期后会开启一个新的会话
 }));
 
 // 响应数据
@@ -22,9 +24,8 @@ app.get('/num', (req, res) => {
         req.session.num = 1;
     }
     req.session.num ++;
-    res.send('req.session.num: ', req.session.num);
+    res.send('req.session.num: ' + req.session.num);
 });
-
 
 // 自定义中间件测试完毕
 app.listen(3000, function() {
